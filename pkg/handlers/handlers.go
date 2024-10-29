0297dc89bbeb1a49b05d315722915d698e4e4d4f
mlvieira <54179980+mlvieira@users.noreply.github.com>
2024-10-29 20:32:19 -0300

    Implement missing handlers

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/mlvieira/bookings/pkg/config"
@@ -28,16 +30,69 @@ func NewHandlers(r *Repository) {
 	Repo = r
 }
 
-// Home is the homepage page handler
-func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-
-	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+func (m *Repository) Landing(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "landing.page.html", &models.TemplateData{})
 }
 
-func (m *Repository) Landing(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "landing.page.html", &models.TemplateData{})
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "contact.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) RoomMajors(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["room_name"] = "Major's Suite"
+	stringMap["image_path"] = "/static/images/marjors-suite.png"
+	stringMap["room_url"] = "majors-suite"
+
+	render.RenderTemplate(w, r, "rooms.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
+
+func (m *Repository) RoomGenerals(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["room_name"] = "General's quarter"
+	stringMap["image_path"] = "/static/images/generals-quarters.png"
+	stringMap["room_url"] = "generals-quarter"
+
+	render.RenderTemplate(w, r, "rooms.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
+
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "availability.page.html", &models.TemplateData{})
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available",
+	}
+
+	out, err := json.Marshal(resp)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start_date")
+	end := r.Form.Get("end_date")
+
+	w.Write([]byte(fmt.Sprintf("Posted. Start date %s and End date is %s", start, end)))
+}
+
+func (m *Repository) Booking(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "reservation.page.html", &models.TemplateData{})
 }
 
 // About is the about page handler
@@ -49,7 +104,7 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIP
 
-	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+	render.RenderTemplate(w, r, "about.page.html", &models.TemplateData{
 		StringMap: stringMap,
 	})
 }
