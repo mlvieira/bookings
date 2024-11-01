97becb4b0691fc2cb72aeda91062a7513dbfd5a9
mlvieira <54179980+mlvieira@users.noreply.github.com>
2024-11-01 10:31:05 -0300

    Simplify main;

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
-	"github.com/alexedwards/scs/v2"
 	"github.com/mlvieira/bookings/internal/config"
-	"github.com/mlvieira/bookings/internal/handlers"
-	"github.com/mlvieira/bookings/internal/models"
 	"github.com/mlvieira/bookings/internal/render"
+	"github.com/mlvieira/bookings/internal/routes"
 )
 
 var app config.AppConfig
-var session *scs.SessionManager
 
 // main is the main logic
 func main() {
@@ -24,43 +19,28 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Starting aplication on http://localhost%s\n", config.PortNumber)
+	fmt.Printf("Starting aplication on http://localhost%s\n", app.Port)
 
 	srv := &http.Server{
-		Addr:    config.PortNumber,
-		Handler: routes(&app),
+		Addr:    app.Port,
+		Handler: routes.Routes(&app),
 	}
 
-	err = srv.ListenAndServe()
-	log.Fatal(err)
+	log.Fatal(srv.ListenAndServe())
 }
 
 func run() error {
 
-	gob.Register(models.Reservation{})
-
-	app.InProduction = false
-
-	session = scs.New()
-	session.Lifetime = 24 * time.Hour
-	session.Cookie.Persist = true
-	session.Cookie.SameSite = http.SameSiteLaxMode
-	session.Cookie.Secure = false
-
-	app.Session = session
+	app = *config.SetupAppConfig(false)
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
-		return err
 	}
 
 	app.TemplateCache = tc
 	app.UseCache = app.InProduction
-
-	repo := handlers.NewRepo(&app)
-	handlers.NewHandlers(repo)
-	render.NewTemplates(&app)
+	app.Port = ":8080"
 
 	return nil
 }
